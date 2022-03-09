f26c0d8082e54a2787374115e8f9fe906a3b0b54
YonatanRojas <yonatanrojaszero@gmail.com>
2022-03-09 14:04:54 -0300

    Calculadora 1.1

---
+++ calculadora function.js
@@ -0,0 +1,33 @@
+class Calculadora {
+    sumar(num1, num2) {
+        return num1 + num2;
+    }
+
+    restar(num1, num2) {
+        return num1 - num2;
+    }
+
+    dividir(num1, num2) {
+        return num1 / num2;
+    }
+
+    multiplicar(num1, num2) {
+        return num1 * num2;
+    }
+} 
+
+/* this part required a simply  understand of the math */
+
+
+/* beta
+class Calculadora {
+    power(num1, num2){
+        return num1 math() num2;
+    }
+    raizCuadrada(num1, num2){
+        return num1 math() num2;
+    }
+    raizCubica(num1, num2){
+        return num1 math() num2;
+    }
+}*/
